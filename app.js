a923159e1f4b1dbaa6cc2a3955c7b67a0b211dc8
Vladimir <vladimirkrizhanovski@gmail.com>
2020-08-04 13:31:15 +0700

    add app.js

---
+++ app.js
@@ -0,0 +1,46 @@
+// 'use strict';
+
+// a = 51
+// console.log(a);
+
+// let number = 5
+// const left = 1
+
+// left = 10
+
+// console.log(left);
+
+// let obj = {
+//     name: 'Jhone',
+//     age: 15,
+//     isMarried: false
+
+// }
+
+// // console.log(obj.name);
+// console.log(obj['name']);
+
+// let arr = ['plum.png', 'orange.png'];
+// console.log(arr[1]);
+
+// const result = confirm('are you ok')
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18 лет?', '')  // + МЕНЯЕТ НА ЧИСЛОВОЕ ЗНАЕНИЕ 
+// console.log(answer + 5);
+
+// const answers = []
+
+// answers[0] = prompt('Как ваше имя', '')
+// answers[1] = prompt('Фамилия', '')
+// answers[2] = prompt('Сколько вам лет', '')
+
+// document.write(answers);
+
+let incr = 10,
+    decr = 10;
+
+incr++
+decr--
+console.log(incr);
+console.log(decr);
